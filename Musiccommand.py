9e6736d9580c8bd3dd2bc4b178b07013b757f6ad
iFanpS <89406191+iFanpSGTS@users.noreply.github.com>
2022-05-20 17:28:28 +0700

    Fix skip command (bug : Always skip the song even just 1 song in queue)

--- Musiccommand.py
+++ Musiccommand.py
@@ -185,7 +185,7 @@ class Music(commands.Cog):
                     await ctx.send("No one react/agree, song will not skipped.")
                 if len(reaction) == 2:
                     await ctx.send("Just 2 people react, song will not skipped.")
-                if len(reaction) >= 1:
+                if len(reaction) >= 3:
                     for i in range(index - 1):
                         del(Queue[ctx.guild.id][index])
                     voice.stop()
